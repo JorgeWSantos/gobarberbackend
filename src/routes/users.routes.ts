fb6839f2a92afe0c33eec620c19b4033af430c3f
Jorge <jorgews.dev@gmail.com>
2020-11-16 22:22:11 -0300

    feat: 🎸 -> adding file upload on users/avatar route.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -21,4 +25,13 @@ usersRouter.post('/', async (req, res) => {
   }
 });
 
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    console.log(req.file);
+    return res.send();
+  },
+);
 export default usersRouter;
